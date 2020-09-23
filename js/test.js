437e93e407ce3163aacaac2392fa8ad21c269540
lam <ldl020590@yahoo.fr>
2020-09-23 10:18:21 +0200

    First commit

---
+++ js/test.js
@@ -0,0 +1,4 @@
+var hello = "Hello world";
+var n = 0xFF & 0xF;
+console.log(hello);
+console.log(n);
---
+++ js/test.ts
@@ -0,0 +1,6 @@
+const hello: string = "Hello world";
+const n: number = 0xFF & 0xF;
+
+
+console.log(hello);
+console.log(n);
